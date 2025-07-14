c831c02e354ad73d89b993c9502923c73720e07b
Albert Wakuteka <albert.wakuteka@hgs.hiddengeniusproject.org>
2025-07-14 10:28:14 -0400

    first commit

---
+++ playlist_builder.py
@@ -0,0 +1,8 @@
+playlists = ["Through the Wire", "Lay your head on my pillow", "RYD/Dark Red", "Spaceship", "Sweetest Taboo"]
+
+print(playlists[0])
+print(playlists[4])
+print(playlists[-3])
+
+for i in playlists:
+    print(i)
